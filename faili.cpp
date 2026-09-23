d1a63670aae02e1e3f7b83e1dc184e79140d2d6d
kamillapaka <kamillapaka220@gmail.com>
2026-09-23 09:43:37 +0300

    vards uzvarsd vecums

--- faili.cpp
+++ faili.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main(){
 ofstream fails("dati.txt");
-fails << "sveiki!"<<endl;
+cout<<"ievadi savu vardu!"<<endl;
+string name;
+cin >> name;
+
+cout<<"ievadi savu uzvardu!"<<endl;
+string last_name;
+cin >> last_name;
+
+cout<<"ievadi savu vecumu!"<<endl;
+int age;
+cin >> age;
+
+fails << " vards!"<<name<<endl;
+fails << " uzvards!"<<name<<endl;
+fails << "vecums! "<<name<<endl;
 fails.close();
       return 0;
 }
